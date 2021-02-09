11007032f971fee69137b293c4bdda329a67e0c3
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2021-02-09 01:19:17 -0800

    xAxes bye

--- static/js/updates.js
+++ static/js/updates.js
@@ -1,13 +1,9 @@
 let socket = io.connect("http://127.0.0.1:5000");
 
-console.log(chart)
 
 socket.on("message", function(data){
 	if(data["type"] == "history"){
 		for(date of Object.keys(data["data"]).reverse()){
-			console.log(date)
-			console.log(data["data"][date])
-			console.log(data)
 			ChangeValue(date, data["data"][date], chart)
 		}
 	}
