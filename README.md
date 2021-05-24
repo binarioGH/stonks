cac79d88021e14c1e6aebcb5528c8e414a6f73cd
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2021-05-24 12:42:55 -0700

    Added README.txt, added function to select between gainers and losers

---
+++ README.md
@@ -0,0 +1,21 @@
+# STONKS!
+
+Stonks is a platform that teaches you how to trade stocks by letting you play in a simulated environment with no risks.
+
+
+## Instalation
+
+`pip install -r requirements.txt`
+
+import `stonks.sql` into a database.
+
+`.\run.cmd`
+
+
+
+### TODO
+
+- [ ] User registration.
+- [ ] Login with google.
+- [ ] Buy and sell.
+- [ ] Realtime data improvements.
--- static/js/menustocklist.js
+++ static/js/menustocklist.js
@@ -1,11 +1,13 @@
 let socket = io.connect(window.location.origin	);
 let hotlist = document.getElementById("hotul");
 
+let gainers_button = document.getElementById("gainers");
+let losers_button = document.getElementById("losers");
 
 let gainers = [];
 let losers = [];
 let total = [];
-
+let mode = 'gainers'
 socket.on("message", function(data){
 	if(data["type"] === "hotstocks"){
 		//<li class="hotstock"><a href="">OwO</a></li>
@@ -13,16 +15,17 @@ socket.on("message", function(data){
 		let container;
 		let link;
 		let price_data;
+		let this_mode;
 		for(s of Object.keys(data["data"])){
 			//console.log(s)
 			let color;
 			if(data["data"][s]["change"][0] == "+"){
 				color = "rgb(105, 226, 56)";
-				gainers.push(s)
+				this_mode = 'gainer';
 			}
 			else{
 				color = "rgb(255, 46, 25)";
-				losers.push(s)
+				this_mode = 'loser';
 			}
 			//Inserting the element sanitized against xss.
 			container = document.createElement("li");
@@ -37,11 +40,49 @@ socket.on("message", function(data){
 			price_data.innerText =  "$" +  data["data"][s]["price"];
 			container.appendChild(link);
 			link.appendChild(price_data)
-			hotlist.appendChild(container);
+			if(this_mode == 'gainer'){
+				gainers.push(container); 
+			}
+			else if(this_mode == 'loser'){
+				losers.push(container);
+			}
+			//hotlist.appendChild(container);
 			
 		}
+		populate(); 
 	}
 });
 
 socket.send('hotstocks');
 
+function populate(){
+	let list;
+	gainers_button.style.color = 'white';
+	losers_button.style.color = 'white';
+	if(mode == 'gainers'){
+		list = gainers;
+		gainers_button.style.color = '#69e233';
+	}
+	else if(mode == 'losers'){
+		list = losers;
+		losers_button.style.color = '#ff2e19';
+	}
+	hotlist.innerHTML = "";
+	for(stock_obj of list){
+		hotlist.appendChild(stock_obj);
+	}
+	console.log(list);
+}
+
+
+
+
+gainers_button.addEventListener("click", function(){
+	mode = 'gainers';
+	populate();
+});
+
+losers_button.addEventListener("click", function(){
+	mode = 'losers';
+	populate();
+});
